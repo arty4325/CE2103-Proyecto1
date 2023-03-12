acb74b71d0504f788c6714f6f7e4dcbe26dd88d6
arty4325 <arturo.acuna.duran@gmail.com>
2023-03-12 12:15:44 -0600

    Se creo una ventana en SDL2

---
+++ cmake-build-debug/CMakeFiles/Proyecto_1_BattleSpace.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_1_BattleSpace.dir/main.cpp.o"
+  "CMakeFiles/Proyecto_1_BattleSpace.dir/main.cpp.o.d"
+  "Proyecto_1_BattleSpace"
+  "Proyecto_1_BattleSpace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_1_BattleSpace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
